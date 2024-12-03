513d130ef73159b8f4029302e148271d4ddee3f4
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-03 11:07:21 -0300

    Convencao-no-nome-de-variaveis

---
+++ Secao-04-Estruturas-de-Programacao/Convencao-no-nome-de-variaveis.js
@@ -0,0 +1,13 @@
+
+//let 3nome = "teste" // SyntaxError: identifier starts immediately after numeric literal
+
+let nome3 = 'teste';
+let $nome = 'teste2'; 
+let _nome = 'teste3';
+
+//let @nome = 'teste'; // SyntaxError: illegal character U+0040
+//let !nome = 'teste'; // SyntaxError: illegal character U+0040
+
+let Nome = 'teste';
+let seuNome = 'teste';
+let seu_nome = 'teste';
