0f6722a2cc0723122c0b78dd70cd04e1e1b11522
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-03 13:25:36 -0300

    Funcao built in console log

---
+++ Secao-04-Estruturas-de-Programacao/Funcao-built-in-console-log.js
@@ -0,0 +1,9 @@
+
+let idade = 39;
+let nome = "Heberton"
+
+console.log(idade); // 39
+console.log(nome); // Heberton 
+
+console.log(`O meu nome é ${nome}, e tenho ${idade} anos`);
+// O meu nome é Heberton, e tenho 39 anos
