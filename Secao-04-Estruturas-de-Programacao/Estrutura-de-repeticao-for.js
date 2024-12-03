7230a5361b18ff3ce3b206630dd73bde754fd5c1
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-03 15:57:24 -0300

    Estrutura de repeticao for

---
+++ Secao-04-Estruturas-de-Programacao/Estrutura-de-repeticao-for.js
@@ -0,0 +1,18 @@
+
+for(let i = 0; i < 100; i = i + 3) {
+
+    console.log(`A soma de i com 2 é: ${i + 2}`);
+
+}
+
+/*
+A soma de i com 2 é: 2
+A soma de i com 2 é: 5
+A soma de i com 2 é: 8
+A soma de i com 2 é: 11
+A soma de i com 2 é: 14
+A soma de i com 2 é: 17
+A soma de i com 2 é: 20
+...
+ */
+
