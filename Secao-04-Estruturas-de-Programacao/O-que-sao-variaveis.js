c86ca301e46791cc7480c7791f303bc500880867
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-03 10:21:58 -0300

    O que são variáveis

--- Secao-04-Estruturas-de-Programacao/O-que-sao-variaveis.js
+++ Secao-04-Estruturas-de-Programacao/O-que-sao-variaveis.js
@@ -18,5 +18,5 @@ console.log(laranja); // 83284
 laranja = "Laranja"
 console.log(laranja); // Laranja
 
-let um = 1,  dois = 2 , três = 3;
-console.log(um + dois + três); // 6
+let um = 1,  dois = 2 , tres = 3;
+console.log(um + dois + tres); // 6
