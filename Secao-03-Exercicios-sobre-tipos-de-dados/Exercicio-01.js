c161f66a51e4287e203e6d19a8e46c5a3b2d6c8f
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-03 07:12:47 -0300

    first commit

---
+++ Secao-03-Exercicios-sobre-tipos-de-dados/Exercicio-01.js
@@ -0,0 +1,10 @@
+
+console.log("Hello World!");
+console.log('Meu nome é Heberton');
+console.log(`Este aqui é o template literals`);
+
+/*
+Hello World!
+Meu nome é José
+Este aqui é o template literals
+*/
