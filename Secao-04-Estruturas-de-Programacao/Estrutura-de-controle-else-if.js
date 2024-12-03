377aed9646a447c867e2315426f7a0c57011eca0
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-03 14:34:13 -0300

    Estrutura de controle else-if

---
+++ Secao-04-Estruturas-de-Programacao/Estrutura-de-controle-else-if.js
@@ -0,0 +1,20 @@
+
+let nome = "Heberton";
+let idade = 39 ;
+
+if (nome != undefined && nome == "Sylford Walker") {
+    console.log("Nome etá definido");
+} else if (nome == "Heberton" && nome.length > 5 && idade == 39 ) {
+    console.log("Nome é Heberton"); 
+} else {
+    console.log("Não é Heberton!");
+}
+
+if(1 > 2) {
+    console.log("Teste");
+} else if(1 == 1) {
+    console.log("Testando");
+}
+
+// Nome é Heberton
+// Testando
