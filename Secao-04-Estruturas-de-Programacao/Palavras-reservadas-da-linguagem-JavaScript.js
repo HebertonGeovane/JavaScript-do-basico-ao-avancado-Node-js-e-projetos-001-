d53820068634d71810a96f1c56633ecb10adb65a
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-03 11:18:01 -0300

    Palavras reservadas da linguagem JavaScript

---
+++ Secao-04-Estruturas-de-Programacao/Palavras-reservadas-da-linguagem-JavaScript.js
@@ -0,0 +1,9 @@
+
+// let if = 'Teste'; // SyntaxError: unexpected token: keyword 'if'
+// let function = 'teste'; // SyntaxError: unexpected token: keyword 'function'
+
+let functionTest = 'teste';
+let function1 = 'teste2';
+
+console.log(functionTest); // teste
+console.log(function1); // teste2
