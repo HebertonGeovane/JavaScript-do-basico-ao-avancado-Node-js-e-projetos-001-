7208a3a6e5e1d4e9486bf7fb4d6d8a6091fafdd9
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-04 09:20:19 -0300

    Exercicio-11

---
+++ Secao-05-Exercicios-estrutura-de-programacao/Exercicio-11.js
@@ -0,0 +1,16 @@
+
+let idade = 17;
+
+let cnh = false;
+
+if(idade >= 18 &&  cnh == true) {
+    console.log("você pode dirigir !!");
+} else if ( idade >= 18 &&  cnh == false) {
+    console.log("você não pode dirigir, mas pode fazer a CNH !!");
+} else {
+    console.log("você não pode dirigir !!");
+}
+
+// você não pode dirigir, mas pode fazer a CNH !!
+
+// let idade = 17 você não pode dirigir !! 
