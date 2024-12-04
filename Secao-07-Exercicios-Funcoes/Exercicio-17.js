532e1d1f89b1dab4007652283ccce1253e18424a
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-04 20:11:17 -0300

    Exercicio-17

---
+++ Secao-07-Exercicios-Funcoes/Exercicio-17.js
@@ -0,0 +1,7 @@
+function imprimirIdade (idade){
+console.log(`Você tem ${idade} anos`)
+}
+
+imprimirIdade(20);   // Você tem 20 anos
+imprimirIdade(15);  // Você tem 15 anos
+imprimirIdade(10); // Você tem 10 anos
