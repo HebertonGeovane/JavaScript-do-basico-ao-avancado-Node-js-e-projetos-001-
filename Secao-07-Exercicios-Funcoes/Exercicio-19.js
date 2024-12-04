9ef6f8249297da9236bda93e94821c0a5811c68f
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-04 20:19:35 -0300

    Exercicio-19

---
+++ Secao-07-Exercicios-Funcoes/Exercicio-19.js
@@ -0,0 +1,7 @@
+function numeroAleatorio(num) {
+    return Math.floor(Math.random() * num) + 1;
+}
+
+console.log(numeroAleatorio(10)); // 7
+console.log(numeroAleatorio(100));// 91
+console.log(numeroAleatorio(50));//  29
