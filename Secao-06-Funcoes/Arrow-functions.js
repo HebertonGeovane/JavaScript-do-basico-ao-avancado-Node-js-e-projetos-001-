2d86ad55d6687e5912a78ee79dea557cd723581c
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-04 14:58:04 -0300

    Arrow-functions

---
+++ Secao-06-Funcoes/Arrow-functions.js
@@ -0,0 +1,14 @@
+
+let consoleTeste = () => {
+    console.log("Olá Mundo! ");
+
+};
+
+consoleTeste();             // Olá Mundo!
+
+
+let soma = (a , b) => {
+   return a + b;
+};
+
+console.log(soma(10 ,300));  // 310 
