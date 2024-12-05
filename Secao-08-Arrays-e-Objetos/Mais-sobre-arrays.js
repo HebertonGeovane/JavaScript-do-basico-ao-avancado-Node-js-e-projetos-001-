600e9277402c1a5a6da69902cad175d4068e398b
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-05 09:54:50 -0300

    Mais-sobre-arrays

---
+++ Secao-08-Arrays-e-Objetos/Mais-sobre-arrays.js
@@ -0,0 +1,8 @@
+let arr     = [1, 4, 5, 6, 7];
+let nomes   = ['Heberton', 'Johnny', 'Aléxia'];
+let bool    = [true, false, true];
+
+console.log(arr[1]); //  4
+console.log(nomes[0]); //  Heberton
+console.log(bool[2]); //  true
+console.log(arr[arr.length - 1]); //  7
