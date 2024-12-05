ec3f01c7185392e8c715dec30651a4a2c1b47b27
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-05 17:35:38 -0300

    Arrays-forEach

---
+++ Secao-08-Arrays-e-Objetos/Arrays-forEach.js
@@ -0,0 +1,14 @@
+let nomes = ["Heberton", "Maria", "Josh", "Sylford Walker", "Johnny"];
+
+nomes.forEach(nome => {
+    console.log("O nome é " + nome);
+});
+// nomes[i]
+
+/*
+O nome é Heberton
+O nome é Maria
+O nome é Josh
+O nome é Sylford Walker
+O nome é Johnny
+*/
