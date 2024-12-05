6ae891f371812ee161fa6139b4622e7e1c3772b9
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-05 13:56:55 -0300

    Arrays-push-e-pop

---
+++ Secao-08-Arrays-e-Objetos/Arrays-push-e-pop.js
@@ -0,0 +1,9 @@
+let nomes = ["Heberton", "Johny", "Maria"];
+
+let elelementosRemovidos = nomes.pop();
+console.log(elelementosRemovidos); // Maria
+
+console.log(nomes); // [ 'Heberton', 'Johnny' ]
+
+nomes.push('Sylford Walker');
+console.log(nomes); // [ 'Heberton', 'Johnny', 'Sylford Walker']
