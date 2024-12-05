769dfa1c90510061910d9c3b1e49f68474a6fefb
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-05 08:10:27 -0300

    Exercicio-20

---
+++ Secao-07-Exercicios-Funcoes/Exercicio-20.js
@@ -0,0 +1,16 @@
+function podeEntrarNaAuto(idade) {
+
+    if (idade >= 18) {
+        console.log(" Pode se Matricular  na auto escola ");
+    } else {
+        console.log(" Não pode se Matricular  na auto escola ");
+    }
+}
+
+podeEntrarNaAuto(18); //  Pode se Matricular  na auto escola 
+
+podeEntrarNaAuto(16); // Não pode se Matricular  na auto escola 
+
+podeEntrarNaAuto(17); // Não pode se Matricular  na auto escola 
+
+podeEntrarNaAuto(25); // Pode se Matricular  na auto escola 
