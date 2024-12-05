93a08c2a8538d32b38dd0e89ac53f51b214215ff
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-05 10:21:14 -0300

    O-que-sao-propriedades

---
+++ Secao-08-Arrays-e-Objetos/O-que-sao-propriedades.js
@@ -0,0 +1,9 @@
+let nome = "Sylford Walker";
+
+console.log(nome.length); // 14
+
+let numeros = [1, 23, 34, 5, 6, 7, 8];
+
+console.log(numeros.length);    // 7
+console.log(numeros['length']);// 7
+console.log(numeros[1]); // 23
