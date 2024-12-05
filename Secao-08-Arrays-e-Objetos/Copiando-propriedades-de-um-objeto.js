8c29fa054a706ce79d051e01fc10299a08f4cc47
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-05 13:07:58 -0300

    Copiando-propriedades-de-um-objeto

---
+++ Secao-08-Arrays-e-Objetos/Copiando-propriedades-de-um-objeto.js
@@ -0,0 +1,15 @@
+let carro = {
+    portas: 2,
+    portamalas: '200l',
+    motor: '2.0'
+}
+
+console.log(carro); // Object { portas: 2, portamalas: "200l", motor: "2.0" }
+
+let adicionais = {
+    tetosolar: true,
+    arcondicionado: true,
+}
+
+Object.assign(carro, adicionais);
+console.log(carro); // Object { portas: 2, portamalas: "200l", motor: "2.0", tetosolar: true, arcondicionado: true }
