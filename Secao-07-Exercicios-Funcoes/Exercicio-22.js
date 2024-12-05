a09a3216edccad932a390758c956a6bdedc55f87
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-05 08:39:23 -0300

    Exercicio-22

---
+++ Secao-07-Exercicios-Funcoes/Exercicio-22.js
@@ -0,0 +1,7 @@
+function numeroPos(numNegativo) {
+    return Math.abs(numNegativo);
+}
+
+console.log(numeroPos(-13));  // 13
+console.log(numeroPos(5)); // 5
+console.log(numeroPos(-34.4)); // 34.4
