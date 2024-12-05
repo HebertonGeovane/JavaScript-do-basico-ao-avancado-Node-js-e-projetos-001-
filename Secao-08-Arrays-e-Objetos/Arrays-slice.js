5608702c7fa4c1ac9ca0ae5b67fda0e6b305a1c6
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-05 14:26:38 -0300

    Arrays-slice

---
+++ Secao-08-Arrays-e-Objetos/Arrays-slice.js
@@ -0,0 +1,11 @@
+let nums = [0,1,2,3,4,5,6,7,8,9];
+
+console.log(nums.slice(4,5)); // Array [ 4 ]
+
+console.log(nums.slice(4,6)); // Array [ 4, 5 ]
+
+console.log(nums.slice(2)); // Array(8) [ 2, 3, 4, 5, 6, 7, 8, 9 ]
+
+console.log(nums.slice(-2)); // Array [ 8, 9 ]
+
+console.log(nums.slice(3,-2)); // Array(5) [ 3, 4, 5, 6, 7 ]
