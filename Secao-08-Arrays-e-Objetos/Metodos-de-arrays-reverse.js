e939de8c9cc16180c529abdfd8c314ba0ea28d0e
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-05 17:52:44 -0300

    Metodos-de-arrays-reverse

---
+++ Secao-08-Arrays-e-Objetos/Metodos-de-arrays-reverse.js
@@ -0,0 +1,11 @@
+let carros = ["BMW", "Fiat", "VW", "Renault", "Audi"];
+
+console.log(carros.reverse());
+
+// Array(5) [ 'Audi', 'Renault', 'VW', 'Fiat', 'BMW' ]
+
+let nums = [1,2,3,45,6,7,7];
+
+console.log(nums.reverse());
+
+// Array(7) [ 7, 7, 6, 45, 3, 2, 1 ]
