ec2b99a811078e5f669578e0b8829899e8ac882f
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-05 09:51:06 -0300

    Arrays

---
+++ Secao-08-Arrays-e-Objetos/Arrays.js
@@ -0,0 +1,10 @@
+let arr = [1, 4, 5, 6, 7];
+let nomes = ['Heberton', 'Johnny', 'Aléxia'];
+let bool = [true, false, true];
+
+let misturado = [1, 'Heberton', true];
+
+console.log(arr); // [ 1, 4, 5, 6, 7 ]
+console.log(nomes);// [ 'Heberton', 'Johnny', 'Aléxia' ]
+console.log(bool); // [ true, false, true ]
+console.log(misturado); // [ 1, 'Heberton', true ]
