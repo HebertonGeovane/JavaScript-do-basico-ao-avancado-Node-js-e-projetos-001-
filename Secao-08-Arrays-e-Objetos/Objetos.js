2c00ee2883812edb0895839a76e75e169d343543
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-05 12:46:43 -0300

    Objetos

---
+++ Secao-08-Arrays-e-Objetos/Objetos.js
@@ -0,0 +1,11 @@
+let cachorro = {
+    patas: 4,
+    nome: 'Spike',
+    latir: function () {
+        console.log("Au Au");
+    }
+};
+
+console.log(cachorro.patas);   // 4 
+console.log(cachorro.nome); // Spike 
+cachorro.latir(); // Au Au 
