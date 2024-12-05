ff23c642c18eabd86a6226e1343af0c36e5bdab8
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-05 13:15:56 -0300

    Verificando-as-chaves-de-um-objeto

---
+++ Secao-08-Arrays-e-Objetos/Verificando-as-chaves-de-um-objeto.js
@@ -0,0 +1,9 @@
+let obj = {
+    'chave1': 1,
+    'chave2': 2,
+    'chave3': 3,
+}
+
+console.log(obj); // Object { chave1: 1, chave2: 2, chave3: 3 }
+
+console.log(Object.keys(obj)); // Array(3) [ "chave1", "chave2", "chave3" ]
