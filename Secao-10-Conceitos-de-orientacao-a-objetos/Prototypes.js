a15ecb2ce8e33412921359192e7e955a2c32e98e
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-06 12:11:01 -0300

    Prototypes

---
+++ Secao-10-Conceitos-de-orientacao-a-objetos/Prototypes.js
@@ -0,0 +1,12 @@
+const pessoa = {
+    maos: 2,
+}
+
+console.log(Object.getPrototypeOf(pessoa));
+// [Object: null prototype] {}
+
+console.log(Object.getPrototypeOf(pessoa) === Object.prototype);
+// true
+
+console.log(pessoa.hasOwnProperty('maos'));
+// true
