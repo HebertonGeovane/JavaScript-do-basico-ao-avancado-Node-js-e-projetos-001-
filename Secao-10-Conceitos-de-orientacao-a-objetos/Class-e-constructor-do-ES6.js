d0690e2487edef3d23d2a9705bbc9f035b4bc01b
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-06 12:21:04 -0300

    Class-e-constructor-do-ES6

---
+++ Secao-10-Conceitos-de-orientacao-a-objetos/Class-e-constructor-do-ES6.js
@@ -0,0 +1,13 @@
+class Cachorro {
+    constructor(raca, patas, cor) {
+        this.raca = raca;
+        this.patas = patas;
+        this.cor = cor;
+    }
+}
+
+let labrador = new Cachorro('Labrador', 4, 'Amarelo');
+
+console.log(labrador);
+
+// Cachorro { raca: 'Labrador', patas: 4, cor: 'Amarelo' }
