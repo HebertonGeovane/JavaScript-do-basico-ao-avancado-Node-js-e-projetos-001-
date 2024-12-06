ff5b1d21035ba290eb376609aaa3ddbc1f09e648
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-06 09:13:04 -0300

    Exercicio-33

---
+++ Secao-09-Exercicios-sobre-arrays-e-objetos/Exercicio-33.js
@@ -0,0 +1,9 @@
+let pessoa = {
+    "name": "Heberton",
+    "age": 39,
+    "role": "Programador"
+}
+
+console.log(pessoa.name);// Heberton 
+console.log(pessoa.age); // 39
+console.log(pessoa.role);// Programador
