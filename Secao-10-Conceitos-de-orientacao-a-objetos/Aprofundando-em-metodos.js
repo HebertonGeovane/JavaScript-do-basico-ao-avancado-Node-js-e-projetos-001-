1fe78aa514b85f4094dcdadaff074054a3fd8897
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-06 11:10:30 -0300

    Aprofundando-em-metodos

---
+++ Secao-10-Conceitos-de-orientacao-a-objetos/Aprofundando-em-metodos.js
@@ -0,0 +1,11 @@
+const cachorro = {
+    uivar: function () {
+        console.log("Auuuuu");
+    },
+    
+    rosnar: function () {
+        console.log("grrrrrrr");
+    }
+}
+cachorro.uivar();// Auuuuu
+cachorro.rosnar();// grrrrrrr
