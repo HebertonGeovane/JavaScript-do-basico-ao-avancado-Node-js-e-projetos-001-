693f868bfc189a9c42b816c3d0c4e435eaf936c1
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-07 08:00:19 -0300

    Exercicio-39.js

---
+++ Secao-11-Exercicios-sobre-orientacao-a-objetos/Exercicio-39.js
@@ -0,0 +1,63 @@
+class Carro {
+    constructor(marca, cor, gasolinaRestante, consumo) {
+        this.marca = marca;
+        this.cor = cor;
+        this.gasolinaRestante = gasolinaRestante;
+        this.consumo = consumo;
+    }
+
+    dirigir(km) {
+        let litrosConsumidos = km / this.consumo;
+        this.gasolinaRestante -= litrosConsumidos;
+    }
+
+    abastecer(l) {
+        this.gasolinaRestante += l;
+    }
+}
+
+let carro = new Carro("VW", "Cinza", 100, 14);
+console.log(carro);
+
+/* 
+Object { marca: "VW", cor: "Cinza", gasolinaRestante: 100, consumo: 14 }
+​
+consumo: 14
+​
+cor: "Cinza"
+​
+gasolinaRestante: 102.85714285714286
+​
+marca: "VW"
+*/
+
+carro.dirigir(100);
+console.log(carro);
+
+/*
+Object { marca: "VW", cor: "Cinza", gasolinaRestante: 92.85714285714286, consumo: 14 }
+​
+consumo: 14
+​
+cor: "Cinza"
+​
+gasolinaRestante: 102.85714285714286
+​
+marca: "VW"
+
+*/
+
+carro.abastecer(10);
+console.log(carro);
+
+/*
+Object { marca: "VW", cor: "Cinza", gasolinaRestante: 102.85714285714286, consumo: 14 }
+​
+consumo: 14
+​
+cor: "Cinza"
+​
+gasolinaRestante: 102.85714285714286
+​
+marca: "VW"
+*/
