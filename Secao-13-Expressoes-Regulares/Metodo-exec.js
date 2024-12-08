583d64d2d8c9128ff2443635dec8798caebcbb45
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-08 11:05:10 -0300

    Metodo-exec

--- Secao-13-Expressoes-Regulares/Metodo-exec.js
+++ Secao-13-Expressoes-Regulares/Metodo-exec.js
@@ -2,7 +2,21 @@ const digitos = /\d+/;
 
 console.log(digitos.exec("Tem o número 100 aqui"));
 
-// [ '100', index: 13, input: 'Tem o número 100 aqui', groups: undefined ]
+/*
+
+Array [ "100" ]
+​
+0: "100"
+​
+groups: undefined
+​
+index: 13
+​
+input: "Tem o número 100 aqui"
+​
+length: 1
+
+*/
 
 console.log(digitos.exec("Tem o número aqui"));
 
