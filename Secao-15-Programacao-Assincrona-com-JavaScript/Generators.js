7b02e968ba42bb1fe085aebcf2e2702ede2803ed
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-09 06:31:42 -0300

    Generators

---
+++ Secao-15-Programacao-Assincrona-com-JavaScript/Generators.js
@@ -0,0 +1,32 @@
+function* criadorId() {
+    let id = 0;
+    while(true) {
+        yield id++;
+    }
+}
+
+let criaId = criadorId();
+
+console.log(criaId.next().value);
+console.log(criaId.next().value);
+console.log(criaId.next().value);
+console.log(criaId.next().value);
+console.log(criaId.next().value);
+console.log(criaId.next().value);
+console.log(criaId.next().value);
+
+console.log(criaId.next());
+
+/*
+0
+1
+2
+3
+4
+5
+6
+
+Object { value: 7, done: false }
+​  done: false
+​  value: 7
+*/
