a2f251099101f6eeaee0163aff7cb6618b688a23
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-18 10:09:39 -0300

    Lendo Arquivos com Node

---
+++ Secao-19-Aprendendo-Node.js/Lendo-Arquivos-com-Node/index.js
@@ -0,0 +1,11 @@
+/*      File System     */
+
+let {readFile} = require('fs');
+
+readFile("arquivo.txt", "utf8", (error, text) => {
+    if (error) {
+        throw error;
+    } else {
+        console.log(text);
+    }
+})
